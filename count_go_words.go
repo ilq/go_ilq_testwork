8614882a77df0d2fdab412f5000fa8575914633a
Lazarev iLya <ilqlazar@gmail.com>
2020-05-10 16:49:02 +0300

    Add lock/unlock.

--- count_go_words.go
+++ count_go_words.go
@@ -8,7 +8,36 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
+
+type TotalCount struct {
+    sync.Mutex
+    count int
+}
+
+func (tc *TotalCount) add(n int) {
+    tc.Lock()
+    tc.count += n
+    tc.Unlock()
+}
+
+type GoRoutineCount struct {
+    sync.Mutex
+    count int
+}
+
+func (gt *GoRoutineCount) increment() {
+    gt.Lock()
+    gt.count++
+    gt.Unlock()
+}
+
+func (gt *GoRoutineCount) decrement() {
+    gt.Lock()
+    gt.count--
+    gt.Unlock()
+}
 
 type wordCount struct {
 	url   string
@@ -16,28 +45,29 @@ type wordCount struct {
 }
 
 func main() {
-	var totalCount int = 0
-	c_qnt := make(chan wordCount)
-	gr_count := 0
-	printed_counts := 0
+	totalCount := TotalCount{count: 0}
+	cQnt := make(chan wordCount)
+	grCount := GoRoutineCount{count: 0}
+	printedCounts := 0
 	k := 2
 	urls := getUrls()
 
 	for _, url := range urls {
-		go getWordCount(url, c_qnt, &gr_count)
-		gr_count++
-		if gr_count >= k {
-			receiveCQnt(c_qnt, &totalCount)
-			printed_counts++
-			gr_count--
+		go getWordCount(url, cQnt, &grCount)
+
+		grCount.increment()
+		if grCount.count >= k {
+			receiveCQnt(cQnt, &totalCount)
+			printedCounts++
+			grCount.decrement()
 		}
 	}
 
-	for i := 0; i < (len(urls) - printed_counts); i++ {
-		receiveCQnt(c_qnt, &totalCount)
+	for i := 0; i < (len(urls) - printedCounts); i++ {
+		receiveCQnt(cQnt, &totalCount)
 	}
 
-	fmt.Printf("Total: %d\n", totalCount)
+	fmt.Printf("Total: %d\n", totalCount.count)
 }
 
 func getUrls() []string {
@@ -53,20 +83,20 @@ func getUrls() []string {
 	}
 }
 
-func getWordCount(url string, c_qnt chan wordCount, gr_count *int) {
+func getWordCount(url string, cQnt chan wordCount, grCount *GoRoutineCount) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	count_go := strings.Count(string(body), "go")
-	*gr_count--
-	c_qnt <- wordCount{url, count_go}
+	count := strings.Count(string(body), "go")
+	grCount.decrement()
+	cQnt <- wordCount{url, count}
 }
 
-func receiveCQnt(c_qnt chan wordCount, totalCount *int) {
-	words := <-c_qnt
+func receiveCQnt(cQnt chan wordCount, totalCount *TotalCount) {
+	words := <-cQnt
 	fmt.Printf("Count for %s: %d\n", words.url, words.count)
-	*totalCount += words.count
+	totalCount.add(words.count)
 }
