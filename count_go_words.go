b9076571a70113c4da34696c9e94d5706403a527
Lazarev iLya <ilqlazar@gmail.com>
2020-05-10 16:12:59 +0300

    Refactoring.

--- count_go_words.go
+++ count_go_words.go
@@ -8,54 +8,65 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	// 	"time"
 )
 
-type counts struct {
+type wordCount struct {
 	url   string
 	count int
 }
 
 func main() {
 	var totalCount int = 0
-	c_counts := make(chan counts)
-	reader := bufio.NewReader(os.Stdin)
+	c_qnt := make(chan wordCount)
 	gr_count := 0
-	count_lost_gr := 0
-	k := 5
-	for {
-		url, err := reader.ReadString('\n')
-		if url == "\n" || err == io.EOF {
-			for i := 0; i < count_lost_gr; i++ {
-				count := <-c_counts
-				fmt.Printf("%s: %d\n", count.url, count.count)
-				totalCount += count.count
-			}
-			fmt.Printf("Total count: %d\n", totalCount)
-			break
-		}
-		url = strings.Replace(url, "\n", "", -1)
+	printed_counts := 0
+	k := 2
+	urls := getUrls()
+
+	for _, url := range urls {
+		go getWordCount(url, c_qnt, &gr_count)
 		gr_count++
-		count_lost_gr++
-
-		go func(url string, gr_count *int) {
-			resp, err := http.Get(url)
-			if err != nil {
-				return
-			}
-			defer resp.Body.Close()
-			body, err := ioutil.ReadAll(resp.Body)
-			count_go := strings.Count(string(body), "go")
-			*gr_count--
-			c_counts <- counts{url, count_go}
-		}(url, &gr_count)
-
 		if gr_count >= k {
-			count := <-c_counts
-			count_lost_gr--
-			fmt.Printf("%s: %d\n", count.url, count.count)
-			totalCount += count.count
+			receiveCQnt(c_qnt, &totalCount)
+			printed_counts++
 			gr_count--
 		}
 	}
+
+	for i := 0; i < (len(urls) - printed_counts); i++ {
+		receiveCQnt(c_qnt, &totalCount)
+	}
+
+	fmt.Printf("Total: %d\n", totalCount)
+}
+
+func getUrls() []string {
+	reader := bufio.NewReader(os.Stdin)
+	urls := make([]string, 0)
+	for {
+		url, err := reader.ReadString('\n')
+		if url == "\n" || err == io.EOF {
+			return urls
+		}
+		url = strings.Replace(url, "\n", "", -1)
+		urls = append(urls, url)
+	}
+}
+
+func getWordCount(url string, c_qnt chan wordCount, gr_count *int) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	count_go := strings.Count(string(body), "go")
+	*gr_count--
+	c_qnt <- wordCount{url, count_go}
+}
+
+func receiveCQnt(c_qnt chan wordCount, totalCount *int) {
+	words := <-c_qnt
+	fmt.Printf("Count for %s: %d\n", words.url, words.count)
+	*totalCount += words.count
 }
